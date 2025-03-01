249cfeeb003f60d6c54e1cce0f8c33a97fd7c5fd
J.AADOR <j.aador@hotmail.com>
2025-03-01 19:34:47 +0400

    #Updated feature X

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,49 +1,29 @@
 import {
-    View,
-    Text,
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-  } from 'react-native';
-  import React from 'react';
-  //import Test from './components/Test';
-  //import Scroll from './components/Scroll';
-  import FancyCard from '../components/FancyCard';
-  import ContactList from '../components/ContactList';
-  import NavigationPanel from '../components/NavigationPanel';
-  import Welcome from '../components/Welcome';
-  import Landing from '../components/Landing';
-  import { NativeStackScreenProps } from "@react-navigation/native-stack"
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
+import React from 'react';
+//import Test from './components/Test';
+//import Scroll from './components/Scroll';
 
+import ContactList from '../components/ContactList';
+import NavigationPanel from '../components/NavigationPanel';
+import Welcome from '../components/Welcome';
+import Landing from '../components/Landing';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 export default function Home() {
-  
   return (
-      <SafeAreaView style={styles.main}>
-        <StatusBar backgroundColor="#000000" barStyle="light-content" />
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Welcome />
-          <Landing />
-          <FancyCard />
-          <ContactList />
-        </ScrollView>
-        <NavigationPanel />
-      </SafeAreaView>
-    );
-  }
-
+    <SafeAreaView style={styles.main}>
+     
+}
 
 const styles = StyleSheet.create({
-    main: {
-        backgroundColor: '#000000',
-      },
-    
-      button: {
-        height: 200,
-        width: 20,
-        backgroundColor: '#FFFFFF',
-      },
+  main: {
+    backgroundColor: '#000000',
+  },
 });
-
-
