ace00801937af727e09396a9247575a122dce1c3
J.AADOR <j.aador@hotmail.com>
2025-05-28 20:27:13 +0400

    Switch responsiveness in ContactList to react-native-size-matters

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -11,10 +11,7 @@ import React from 'react';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../src/types';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+import {moderateScale, scale, verticalScale} from 'react-native-size-matters';
 
 //import {SafeAreaView} from 'react-native-safe-area-context';
 
@@ -28,62 +25,12 @@ type Props = {
 };
 
 export default function ContactList() {
-  const navigation = useNavigation<NavigationProp>();
   const contacts = [
     {
       uid: 1,
-      name: 'Andrew James',
-      phone: '+97158987423',
-      imageUrl:
-        'https://play-lh.googleusercontent.com/7Ak4Ye7wNUtheIvSKnVgGL_OIZWjGPZNV6TP_3XLxHC-sDHLSE45aDg41dFNmL5COA=w240-h480-rw',
-    },
-
-    {
-      uid: 2,
-      name: 'Jason Mamoa',
-      phone: '+97158987423',
-      imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW-OU9SvLHba0VWc2LyIZSTzKaY2t5R7-koA&s',
-    },
-
-    {
-      uid: 3,
-      name: 'Laran David',
-      phone: '+97158987423',
-      imageUrl:
-        'https://wp-socialnation-assets.s3.ap-south-1.amazonaws.com/wp-content/uploads/2023/07/21160619/318869478_689273422811789_4690697243876215757_n-819x1024.jpg',
-    },
-
-    {
-      uid: 4,
-      name: 'Lexum Berg',
-      phone: '+97158987423',
-      imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVLH-gsrpMgAtFIvkCh2-_SkytOJNSdMTGQg&s',
-    },
-
-    {
-      uid: 5,
-      name: 'Hannah Stocking',
-      phone: '+97157465825',
-      imageUrl:
-        'https://img.freepik.com/premium-photo/ai-avatar-girl-photo_940072-112.jpg',
-    },
-
-    {
-      uid: 6,
-      name: 'Albert Libbo',
-      phone: '+97157465825',
-      imageUrl:
-        'https://readwrite.com/wp-content/uploads/2024/04/synthesia-900x506.png',
-    },
-
-    {
-      uid: 7,
-      name: 'linda Sarry',
-      phone: '+97157465825',
-      imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHRmGljLHsGHa8Mv6MVKv3BvHwMPHHqNO4XQ&s',
+      name: 'October',
+      cast: 'Varun Dhawan, Kriti Sanon',
+      time: '9.15 PM',
     },
   ];
 
@@ -91,82 +38,78 @@ export default function ContactList() {
     <SafeAreaView style={styles.main}>
       <View style={styles.container}>
         <ScrollView nestedScrollEnabled showsVerticalScrollIndicator={false}>
-          {contacts.map(({uid, name, phone, imageUrl}) => (
+          {contacts.map(({uid, name, cast, time}) => (
             <View key={uid} style={styles.contacts}>
-              <Image style={styles.contactImage} source={{uri: imageUrl}} />
               <View style={styles.nameplate}>
-                <Text style={styles.userName}>{name}</Text>
-                <Text style={styles.phone}>{phone}</Text>
+                <Text style={styles.moviename}>{name}</Text>
+                <Text style={styles.cast}>{cast}</Text>
+              </View>
+              <View style={styles.timebar}>
+                <Text style={styles.time}>{time}</Text>
               </View>
             </View>
           ))}
         </ScrollView>
       </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Details')}>
-        <Text style={styles.continuetitle}>Log Out</Text>
-      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    height: hp('5%'),
-    width: wp('90%'),
-    borderRadius: hp('3%'),
+    height: verticalScale(40),
+    width: scale(250),
+    borderRadius: moderateScale(20),
     backgroundColor: 'rgb(255, 174, 0)',
     alignItems: 'center',
     justifyContent: 'center',
   },
-
   container: {
-    padding: 10,
-
-    //alignItems: 'center',
+    padding: moderateScale(10),
     backgroundColor: '#dceafa',
     overflow: 'scroll',
-    height: hp('30%'),
-    width: wp('90%'),
-    borderRadius: hp('3%'),
-    //marginBottom: 100,
+    height: verticalScale(120),
+    width: scale(300),
+    borderRadius: moderateScale(20),
   },
   contacts: {
     flexDirection: 'row',
-    padding: 10,
-    margin: 5,
-    //alignItems: 'center',
+    padding: moderateScale(10),
+    margin: moderateScale(5),
     backgroundColor: '#FFFFFF',
     overflow: 'hidden',
-    borderRadius: 10,
+    borderRadius: moderateScale(10),
   },
   contactImage: {
-    height: 50,
-    width: 50,
-    borderRadius: 25,
+    height: scale(50),
+    width: scale(50),
+    borderRadius: moderateScale(25),
   },
-
   nameplate: {
-    marginLeft: 10,
+    marginLeft: moderateScale(10),
   },
-
-  userName: {
-    fontSize: wp('2.8%'),
+  moviename: {
+    fontSize: moderateScale(14),
     fontWeight: 'bold',
   },
-  phone: {},
-
+  cast: {},
   continuetitle: {
-    fontSize: wp('4%'),
-
+    fontSize: moderateScale(18),
     fontWeight: 'bold',
   },
-
   main: {
     justifyContent: 'space-evenly',
     alignItems: 'center',
-    height: hp('50%'),
-    marginBottom: 100,
+    height: verticalScale(200),
+    marginBottom: moderateScale(40),
+  },
+  time: {
+    fontSize: moderateScale(14),
+    fontWeight: 'normal',
+  },
+  timebar: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    flex: 1,
   },
 });
